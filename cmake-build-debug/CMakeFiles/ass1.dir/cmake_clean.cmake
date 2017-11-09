cc1b62b651bd9db4329bc2c02ca035e8b230d8a8
Unknown <chaimc1@gmail.com>
2017-11-09 21:16:59 +0200

    update

---
+++ cmake-build-debug/CMakeFiles/ass1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ass1.dir/main.cpp.o"
+  "CMakeFiles/ass1.dir/BaseFile.cpp.o"
+  "CMakeFiles/ass1.dir/File.cpp.o"
+  "CMakeFiles/ass1.dir/Directory.cpp.o"
+  "ass1.pdb"
+  "ass1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ass1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
