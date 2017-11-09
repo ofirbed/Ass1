9b242aec440f7cdb0587374a2260980f84d7dca4
Unknown <ofirbed@post.bgu.ac.il>
2017-11-09 10:39:22 +0200

    upload proj

---
+++ cmake-build-debug/CMakeFiles/Ass1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ass1.dir/main.cpp.o"
+  "Ass1.pdb"
+  "Ass1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ass1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
